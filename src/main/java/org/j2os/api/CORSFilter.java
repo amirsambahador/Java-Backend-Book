8a8545f2fa8fcc5f46fddcc0ed44392e96fa86da
amirsam <amirsam@j2os.org>
2026-02-19 09:56:43 -0800

    اضافه کردن کامنت در کد

--- src/main/java/org/j2os/api/CORSFilter.java
+++ src/main/java/org/j2os/api/CORSFilter.java
@@ -4,7 +4,9 @@ import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
-
+/*
+    CORSFilter for Access-Control-Allow-Origin
+ */
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
 
